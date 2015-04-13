09088da61723d441b398af03a4373efc318ad1b1
kentarou kanno <kanno@kentarou.me>
2015-04-14 05:00:39 +0900

    firstCommit

---
+++ Review/Review/AppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.m
+//  Review
+//
+//  Created by KentarOu on 2014/08/20.
+//  Copyright (c) 2014年 KentarOu. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    return YES;
+}
+							
+
+@end
